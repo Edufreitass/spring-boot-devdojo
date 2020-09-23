4a070c5bf0587e26b64d14c672df58333db2af2c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-09-22 22:58:59 -0300

    feat: Padrões REST e PUT e DELETE pt 03

--- springboot-essentials/src/main/java/br/com/devdojo/controller/StudentController.java
+++ springboot-essentials/src/main/java/br/com/devdojo/controller/StudentController.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,19 @@ public class StudentController {
 		Student.studentList.add(student);
 		return new ResponseEntity<>(student, HttpStatus.OK);
 
+	}
+
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		Student.studentList.add(student);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 }
