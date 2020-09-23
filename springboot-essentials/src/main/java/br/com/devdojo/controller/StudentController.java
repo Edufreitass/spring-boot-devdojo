d727af1649c60c0936f873d65e9dc47df6c0e7e2
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-09-22 22:27:58 -0300

    feat: Padrões REST e POST pt 01

--- springboot-essentials/src/main/java/br/com/devdojo/controller/StudentController.java
+++ springboot-essentials/src/main/java/br/com/devdojo/controller/StudentController.java
@@ -1,28 +1,43 @@
 package br.com.devdojo.controller;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
 import br.com.devdojo.util.DateUtil;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/students")
 public class StudentController {
 
-	@Autowired
-	private DateUtil dateUtil;
+	private final DateUtil dateUtil;
 
-	@GetMapping("/list")
-	public List<Student> listAll() {
+	public StudentController(DateUtil dateUtil) {
+		this.dateUtil = dateUtil;
+	}
+
+	@GetMapping
+	public ResponseEntity<?> listAll() {
 		System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return Arrays.asList(new Student("Deku"), new Student("Todoroki"));
+		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+		Student student = new Student();
+		student.setId(id);
+		int index = Student.studentList.indexOf(student);
+		if (index == -1) {
+			return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
 
 }
--- springboot-essentials/src/main/java/br/com/devdojo/model/Student.java
+++ springboot-essentials/src/main/java/br/com/devdojo/model/Student.java
@@ -1,8 +1,18 @@
 package br.com.devdojo.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
 
+	private int id;
 	private String name;
+	public static List<Student> studentList;
+
+	static {
+		studentRepositosy();
+	}
 
 	public Student() {
 	}
@@ -11,12 +21,51 @@ public class Student {
 		this.name = name;
 	}
 
+	public Student(int id, String name) {
+		this(name);
+		this.id = id;
+	}
+
+	private static void studentRepositosy() {
+		studentList = new ArrayList<>(Arrays.asList(new Student(1, "Deku"), new Student(2, "Todoroki")));
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 }
