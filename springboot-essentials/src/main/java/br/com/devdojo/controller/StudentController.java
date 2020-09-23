9e0b5b5347fe910cd5ba50523c523f71699ecb3c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-09-23 10:02:19 -0300

    feat: Tratamento de erros em REST pt 03 - Transações

--- springboot-essentials/src/main/java/br/com/devdojo/controller/StudentController.java
+++ springboot-essentials/src/main/java/br/com/devdojo/controller/StudentController.java
@@ -3,6 +3,7 @@ package br.com.devdojo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ public class StudentController {
 	}
 
 	@PostMapping
+	@Transactional(rollbackFor = Exception.class)
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		return new ResponseEntity<>(studentDao.save(student), HttpStatus.CREATED);
 
